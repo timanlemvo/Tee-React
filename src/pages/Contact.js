71943c6f99bd329f1c8e06218754994db4cfe306
timanlemvo <timanlemvo@gmail.com>
2021-09-05 15:54:41 -0700

    updated contact pages

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,33 +1,13 @@
 import React from "react";
-import { MDBContainer, MDBRow, MDBCol, MDBIcon } from "mdbreact";
+import ContactInfo from "../components/Contact/index";
+import Wrapper from "../components/Wrapper/index";
 
-const ContactInfo = () => {
+function Contact() {
   return (
-    <MDBContainer className="mt-5">
-      <MDBRow>
-        <MDBCol md="12" className="text-center">
-          <h2>Contact Information</h2>
-          <hr class="solid bg-dark" />
-          <h4>
-            Please feel free to contact me at any time!
-            Thank you!
-          </h4>
-          <hr className="hr-light w-25" />
-          <h3>Cell Phone</h3>
-          <hr className="hr-dark w-25" />
-          <a href="tel:661-492-0497">
-            <MDBIcon icon="phone-alt" /> 661-492-0497
-          </a>
-          <hr className="hr-light w-25" />
-          <h3>E-mail</h3>
-          <hr className="hr-dark w-25" />
-          <a href="mailto:TimaNlemvo@gmail.com">
-            <MDBIcon icon="envelope" /> TimaNlemvo@gmail.com
-          </a>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
+    <Wrapper>
+      <ContactInfo />
+    </Wrapper>
   );
-};
+}
 
-export default ContactInfo;
+export default Contact;
