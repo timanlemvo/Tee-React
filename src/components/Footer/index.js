e6b20dbdca1293944463941da9c4469c0fc7780b
timanlemvo <timanlemvo@gmail.com>
2021-09-05 15:52:41 -0700

    updated and moved a few files to new folder

---
+++ src/components/Footer/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "/style.css";
+
+function Footer() {
+  return (
+    <footer class="page-footer fixed-bottom font-small special-color-dark pt-4">
+      <div class="container">
+        <ul class="list-unstyled list-inline text-center">
+          <li class="list-inline-item">
+            <a
+              class="btn-floating btn-fb mx-1"
+              href="https://github.com/timanlemvo"
+            >
+              <i class="fab fa-github"> </i>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a
+              class="btn-floating btn-li mx-1"
+              href="https://www.linkedin.com/in/tima-nlemvo-820b92b8/"
+            >
+              <i class="fab fa-linkedin-in"> </i>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="footer-copyright text-center py-3">
+        © 2020 Copyright:
+        <a href="https://github.com/timanlemvo">
+          {" "}
+          Tima Nlemvo
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/pages/Portfolio/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+  MDBCardGroup,
+  MDBIcon,
+  MDBContainer,
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBCardTitle,
+  MDBCardText,
+} from "mdbreact";
+
+export default Projects;
