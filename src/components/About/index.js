9ed8656c8615f546c226e1f475f38bf33d0b6e7f
timanlemvo <timanlemvo@gmail.com>
2021-09-05 22:43:31 -0700

    audit fixes

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,9 +8,6 @@ const AboutMe = () => {
         <MDBCol md="12">
           <h2>About Me</h2>
           <hr class="solid bg-dark" />
-          <img 
-          style={{ height: "285px" }}
-          src= ""/>
           <p style={{ fontSize: "27px" }}>
             Hello, my name is Tima Nlemvo. I am actively looking for job opportunities as a Junior Web Developer remotely or in the Los Angeles Area.           </p>
         </MDBCol>
